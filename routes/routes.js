5502998bee077608389a46d43f9700316d30f391
jeremypiercy <jeremy.piercy@rpssoftware.co.uk>
2020-10-15 21:52:21 +0100

    Change routes for get to be /api/__ instead of just /__

--- routes/routes.js
+++ routes/routes.js
@@ -9,40 +9,37 @@ console.dir(userController);
 
 module.exports = (app) => { 
 
-    router.get('/allusers', (req, res) => {
+
+    router.get('/api/allusers', (req, res) => {
         userController.viewAll(app, req, res);
     });
-
-    router.get('/user/:userID', (req, res) => {
+    router.get('/api/user/:userID', (req, res) => {
         userController.viewItem(app, req, res);
     });
-
-    router.post('/api/user', (req, res) => {
+    router.post('/api/api/user', (req, res) => {
         userController.addItem(app, req, res);
     });
-
     router.put('/api/user', (req, res) => {
         userController.amendItem(app, req, res);
     });
-
     router.delete('/api/user', (req, res) => {
         userController.deleteItem(app, req, res);
     });
 
 
-    router.get('/alltransactions/', (req, res) => {
+    router.get('/api/alltransactions/', (req, res) => {
         transactionController.viewAll(app, req, res);
     });
-    router.get('/alltransactions/:userID', (req, res) => {
+    router.get('/api/alltransactions/:userID', (req, res) => {
         transactionController.viewAllForUser(app, req, res);
     });
-    router.get('/incomings/:userID', (req, res) => {
+    router.get('/api/incomings/:userID', (req, res) => {
         transactionController.viewIncomingsForUser(app, req, res);
     });
-    router.get('/outgoings/:userID', (req, res) => {
+    router.get('//apioutgoings/:userID', (req, res) => {
         transactionController.viewOutgoingsForUser(app, req, res);
     });
-    router.get('/transaction/:transactionID', (req, res) => {
+    router.get('/api/transaction/:transactionID', (req, res) => {
         transactionController.viewItem(app, req, res);
     });
     router.post('/api/transaction', (req, res) => {
