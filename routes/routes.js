34fa8e2ff5eaa791fa18cfd28210577ae5edc029
jeremypiercy <jeremy.piercy@rpssoftware.co.uk>
2020-10-15 16:48:01 +0100

    link routes.js to the new controllers

--- routes/routes.js
+++ routes/routes.js
@@ -2,40 +2,62 @@ const express = require('express');
 
 const router = express.Router();
 
-const myControllers = require('../controllers/controllers.js');
+const userController = require('../controllers/usercontroller.js');
+const transactionController = require('../controllers/transactioncontroller.js');
 
-console.dir(myControllers);
+console.dir(userController);
 
-module.exports = (app) => {
+module.exports = (app) => { 
 
-    router.get('/main', (req, res) => {
-        return res.render('main', {
-            title: 'EJS Example from Parts', 
-            message: 'Hello Template built in parts',
-            showMsg: true,
-            headingOne: 'Page made from parts'
-            });
+    router.get('/allusers', (req, res) => {
+        userController.viewAll(app, req, res);
     });
 
-    router.get('/allfilms', (req, res) => {
-        myControllers.viewAll(app, req, res);
+    router.get('/user/:userID', (req, res) => {
+        userController.viewItem(app, req, res);
     });
 
-    router.get('/film/:filmID', (req, res) => {
-        myControllers.viewItem(app, req, res);
+    router.post('/api/user', (req, res) => {
+        userController.addItem(app, req, res);
     });
 
-    router.post('/api/film', (req, res) => {
-        myControllers.addItem(app, req, res);
+    router.put('/api/user', (req, res) => {
+        userController.amendItem(app, req, res);
     });
 
-    router.put('/api/film', (req, res) => {
-        myControllers.amendItem(app, req, res);
+    router.delete('/api/user', (req, res) => {
+        userController.deleteItem(app, req, res);
     });
 
-    router.delete('/api/film', (req, res) => {
-        myControllers.deleteItem(app, req, res);
+
+    router.get('/alltransactions/', (req, res) => {
+        transactionController.viewAll(app, req, res);
     });
+    router.get('/alltransactions/:userID', (req, res) => {
+        transactionController.viewAllForUser(app, req, res);
+    });
+    router.get('/incomings/:userID', (req, res) => {
+        transactionController.viewIncomingsForUser(app, req, res);
+    });
+    router.get('/outgoings/:userID', (req, res) => {
+        transactionController.viewOutgoingsForUser(app, req, res);
+    });
+    router.get('/transaction/:transactionID', (req, res) => {
+        transactionController.viewItem(app, req, res);
+    });
+    router.post('/api/transaction', (req, res) => {
+        transactionController.addItem(app, req, res);
+    });
+    router.put('/api/transaction', (req, res) => {
+        transactionController.amendItem(app, req, res);
+    });
+    router.delete('/api/transaction', (req, res) => {
+        transactionController.deleteItem(app, req, res);
+    });
+    router.get('/alloutgoings', (req, res) => {
+        transactionController.viewAllOutgoings(app, req, res);
+    });
+
 
   
     return router;
