a6c5a5b8885576a6277c21991bb3d1d1caa47d2e
cds-mm <mykhaylo.markov@connecteddata.solutions>
2020-10-16 18:17:52 +0100

    Initial FE login and registrations views

---
+++ src/components/login.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <form>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" v-model="form.username" required>
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" v-model="form.password" required>
+            </div>
+            <button type="button" class="btn btn-outline-dark" @click="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            form: {
+                username: '',
+                password: ''
+            }
+        }
+    },
+
+    methods:{
+        submit(){
+            console.log("Login button clicked")
+        }
+    }
+}
+</script>
