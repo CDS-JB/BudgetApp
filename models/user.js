370612836049d2769006068c9e9c4292abb4e4a7
jeremypiercy <jeremy.piercy@rpssoftware.co.uk>
2020-11-18 17:04:33 +0000

    remove unused code and start adding code to update user

--- models/user.js
+++ models/user.js
@@ -3,137 +3,100 @@ const saltRounds = 10;
 
 async function login(app, emailAddress, password, session)
 {
-    var users = await getUsersWithEmailAddress(app, emailAddress);
-    if (users.length > 0) {
-        console.log("User found in database for " + emailAddress)           
-        bcrypt.compare(password, users[0].password, function(err, result) 
-        {
-            if (result == true) {
-                session.login = true;                                  
-                session.userId = users[0]._id;                            
-                console.log("password correct")
-                return({ statusCode: 200, msg: "Found " + emailAddress  })
-            }
-            else {
-                console.log("password incorect")
-                return({ statusCode: 400, msg: "Invalid Password" })
-            }
-        });
-
-    }
-    else {
-
-        console.log("User not found in database for " + emailAddress)             
-        return({ statusCode: 400,  msg: "User Not Found" });   
-    }
-
-
-    // app
-    //     .set("myDb")
-    //     .collection("usersCollection")
-    //     .find({ emailAddress: emailAddress })
-    //     .toArray(function (err, docs) {
-    //     if (err) {
-    //       console.error(err);
-    //     }
-    //     if (docs.length > 0) {            
-    //         console.log("User found in database for " + emailAddress)           
-    //         bcrypt.compare(password, docs[0].password, function(err, result) 
-    //         {
-    //             console.info(result);
-    //             if (result == true) {
-    //                 req.session.login = true;                                  
-    //                 req.session.userId = docs[0]._id;                            
-    //                 console.log("password correct")
-    //                 res.json({ statusCode: 200, msg: "Found " + emailAddress  })
-    //             }
-    //             else {
-    //                 console.log("password incorect")
-    //                 res.json({ statusCode: 400, msg: "Invalid Password" })
-    //             }
-    //         });
-    //     }
-    //     else {                        
-    //         console.log("User not found in database for " + emailAddress)             
-    //         res.json({ statusCode: 400,  msg: "User Not Found" });            
-    //     }    
-    // })    
-}
-
-function getUsersWithEmailAddress(app, emailAddress ) {        
-    return new Promise (resolve => {
-        console.log("email: ", emailAddress) ;
+    return new Promise (resolve =>  {        
         app
         .set("myDb")
         .collection("usersCollection")
         .find({ emailAddress: emailAddress })
         .toArray(function (err, users) {
             if (err) {
-                console.error(err);
+                console.error(err);                
+            } 
+            if (users.length > 0) {
+                let user = users[0];
+                bcrypt.compare(password, user.password, function(err, result) {       
+                    if (result == true) {
+                        session.login = true;                                  
+                        session.userId = user._id;                            
+                        console.log("password correct");
+                        resolve({ statusCode: 200, msg: "Found " + user.emailAddress  });
+                    }
+                    else {
+                        console.log("password incorect");
+                        resolve({ statusCode: 400, msg: "Invalid Password" });
+                    }
+                });
             }
-          //  console.dir("Model users" + docs); 
-            resolve( users);  
+            else {
+                console.log("User not found in database for " + emailAddress);             
+                resolve({ statusCode: 400,  msg: "User Not Found" });   
+            } 
         });
+    });   
+}
+
+function addUser(app,newUser)  {     
+    return new Promise (resolve =>  {
+        bcrypt.hash(newUser.password, saltRounds, function (err, hash) 
+        {
+            let hashedPwd = hash;
+            console.log(newUser.password);
+            newUser.password = hashedPwd;
+            console.log(hashedPwd);
+
+            app
+            .get('myDb')
+            .collection("usersCollection")
+            .insertOne(newUser, function (err, dbResp) {
+                if (err) {
+                    console.error(err);
+                }
+                if (dbResp.insertedCount === 1) {
+                    console.info( "Successfully Added" + dbResp.insertedId);
+                    resolve ({ statusCode: 200, msg: "Successfully Added user" });               
+                } else {
+                    console.log("Failed to add to db");
+                    resolve({ statusCode: 400,msg: "Failed to add to db" });                
+                }
+            });
+        }); 
     });
 }
 
-
-function getUserWithUserId(app, userId ) {        
-    return new Promise (resolve => {
-        console.log("email: ", emailAddress) ;
-        app
-        .set("myDb")
-        .collection("usersCollection")
-        .find({ _id: userId })
-        .toArray(function (err, docs) {
-            if (err) {
-                console.error(err);
+function updateUser(app, userInfo, userId) {   
+    return new Promise (resolve =>  {
+        console.info("PUT / UPDATE controller")
+        var o_id = new ObjectId(userId);
+        console.info(o_id);
+        app.get('myDb').collection("usersCollection").updateOne(
+            { _id: o_id },
+            { $set : userInfo},
+            function (err, dbResp) {
+                if (err) {
+                    console.error(err)
+                }
+                if (dbResp.modifiedCount === 1) {
+                    resolve({ msg: "Successfully Amended" })
+                } else {
+                    resolve({ msg: "Not Found" })
+                }
             }
-            console.dir("Model users" + docs); 
-            resolve( {user : docs} );  
-        });
-    });        
+        );        
+    });
 }
-
-function getAllUsers(app)
-{        
-    return new Promise (resolve => {
-        app
-        .set("myDb")
-        .collection("usersCollection")    
-        .find()
-        .toArray(function (err, docs) {
-            if (err) {
-                console.error(err);
-            }             
-            resolve( docs );
-        });
-    });        
+function createUserFromRequest(req)
+{
+    let newUser =   {
+        firstName: req.body.firstname, 
+        surname: req.body.lastname,
+        dateOfBirth : req.body.dateOfBirth, 
+        emailAddress: req.body.username,
+        password: req.body.password
+      };
+      return newUser;
 }
-
-    //this will add new user to the user collection  
-    //this will take 2 parameter.newUser is object and cb is a callback  
-function addUser(app,newUser,res)  {      
-    app
-        .get('myDb')
-        .collection("usersCollection")
-        .insertOne(newUser, function (err, dbResp) {
-            if (err) {
-                console.error(err)
-            }
-            if (dbResp.insertedCount === 1) {
-                res.json({ statusCode: 200, msg: "Successfully Added" + dbResp.insertedId })
-                console.info( "Successfully Added" + dbResp.insertedId)
-            } else {
-                res.json({ statusCode: 400,msg: "Failed to add to db" })
-                console.info("Failed to add to db")
-            }
-        })
-}
-    
    
 module.exports.login = login;
-module.exports.getAllUsers = getAllUsers;
-module.exports.getUsersWithEmailAddress = getUsersWithEmailAddress;
-module.exports.getUserWithUserId = getUserWithUserId;
 module.exports.addUser = addUser;
+module.exports.updateUser = updateUser;
+module.exports.createUserFromRequest = createUserFromRequest;
