ea492bc6c6853813489860f893c2adf7025a8876
jeremypiercy <jeremy.piercy@rpssoftware.co.uk>
2020-11-21 20:54:14 +0000

    clean up unused routes

--- routes/routes.js
+++ routes/routes.js
@@ -13,10 +13,14 @@ module.exports = (app) => {
 
     router.get('/api/report', (req, res) => {report.show(app, req, res)})
 
+
     router.post('/api/login', (req, res) => {
         userController.login(app, req, res);
     });
-    router.post('/api/register', (req, res) => {
+    router.post('/api/logout', (req,res) => {
+        userController.logout(app, req, res);
+    })
+    router.put('/api/register', (req, res) => {
         userController.addItem(app, req, res);
     });
 
@@ -25,7 +29,6 @@ module.exports = (app) => {
     router.get('/api/transactions', (req, res) => {
         transactionController.viewAllForUser(app, req, res);
     });
-
     router.put('/api/addtransaction', (req, res) => {
         transactionController.addItem(app, req, res);
     });
@@ -39,48 +42,5 @@ module.exports = (app) => {
         paymentController.addItem(app, req, res);
     });
 
-    // router.get('/api/allusers', (req, res) => {
-    //     userController.viewAll(app, req, res);
-    // });
-    // router.get('/api/user/:userID', (req, res) => {
-    //     userController.viewItem(app, req, res);
-    // });
-    // router.post('/api/user', (req, res) => {
-    //     userController.addItem(app, req, res);
-    // });
-    // router.put('/api/user', (req, res) => {
-    //     userController.amendItem(app, req, res);
-    // });
-    // router.delete('/api/user', (req, res) => {
-    //     userController.deleteItem(app, req, res);
-    // });
-
-
-    // router.get('/api/alltransactions/', (req, res) => {
-    //     transactionController.viewAll(app, req, res);
-    // });
-    // router.get('/api/alltransactions/:userID', (req, res) => {
-    //     transactionController.viewAllForUser(app, req, res);
-    // });
-    // router.get('/api/incomings/:userID', (req, res) => {
-    //     transactionController.viewIncomingsForUser(app, req, res);
-    // });
-    // router.get('/api/outgoings/:userID', (req, res) => {
-    //     transactionController.viewOutgoingsForUser(app, req, res);
-    // });
-    // router.get('/api/transaction/:transactionID', (req, res) => {
-    //     transactionController.viewItem(app, req, res);
-    // });
-    // router.post('/api/transaction', (req, res) => {
-    //     transactionController.addItem(app, req, res);
-    // });
-    // router.put('/api/transaction', (req, res) => {
-    //     transactionController.amendItem(app, req, res);
-    // });
-    // router.delete('/api/transaction', (req, res) => {
-    //     transactionController.deleteItem(app, req, res);
-    // });
-
-  
     return router;
 }
