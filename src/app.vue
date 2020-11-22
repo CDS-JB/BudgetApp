b02f556fd77c0df1039a292b82d4f872f9cb71b2
cds-mm <mykhaylo.markov@connecteddata.solutions>
2020-11-22 14:56:14 +0000

    Navbar: update displayed user name

--- src/app.vue
+++ src/app.vue
@@ -4,7 +4,7 @@
             <nav v-if="displayNav" class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <a class="navbar-brand">
                     <img src="/images/user.png" width="30" height="30" class="d-inline-block align-top" alt="">
-                    Hi {{$session.get('name')}}
+                    Hi {{displayName}}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,7 +23,7 @@
             </nav>
         </header>
         <main>
-            <router-view></router-view>
+            <router-view @updateSession="updateSession"></router-view>
         </main>
     </div>
 </template>
@@ -32,24 +32,39 @@
 export default {
     data(){
         return {
-            displayNav: true
+            displayNav: true,
+            displayName: ''
         }
     },
 
     methods: {
         logout(){
             if(this.$session.exists()){
-                this.$session.destroy()
-                this.$router.push('/')
+                axios.get("/api/logout")
+                    .then((res) => {
+                        this.$session.destroy()
+                        this.$router.push('/')
+                }).catch((err) => {
+                    console.error(err.response.data);
+                })
             }
         },
 
         checkSession(){
             if(this.$session.exists()){
+                this.updateDisplayName()
                 this.displayNav = true
             } else {
                 this.displayNav = false
             }
+        },
+
+        updateSession(){
+            this.updateDisplayName()
+        },
+
+        updateDisplayName(){
+            this.displayName = this.$session.get('name')
         }
     },
 
