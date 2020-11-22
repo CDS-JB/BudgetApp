563ab1bc49ca71318f4d10e82fb64f5d6c31b390
cds-mm <mykhaylo.markov@connecteddata.solutions>
2020-11-22 14:55:20 +0000

    Budget: check $session exists

--- src/components/budget.vue
+++ src/components/budget.vue
@@ -4,6 +4,11 @@
 
 <script>
 export default {
-    
+    beforeCreate: function () {
+        if (!this.$session.exists()){
+            console.error('Unauthenticated. Redirecting to Welcome')
+            this.$router.push('/')
+        }
+    }
 }
 </script>
