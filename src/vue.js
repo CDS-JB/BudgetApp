787b287ecba952c385b5a9588f2420d0c3f208a4
cds-mm <mykhaylo.markov@connecteddata.solutions>
2020-11-22 13:46:48 +0000

    About Us, Budet, Settings views

--- src/vue.js
+++ src/vue.js
@@ -10,7 +10,10 @@ import NotFound from './components/notFound';
 import Report from './components/report';
 import Login from './components/login';
 import Register from './components/register';
-import Home from './components/home';
+// import Home from './components/home';
+import Budget from './components/budget';
+import Settings from './components/settings';
+import AboutUs from './components/aboutUs';
 import Template from './components/template';
 
 // Import App
@@ -31,8 +34,11 @@ const router = new VueRouter({
         {path: '/', component: Welcome, meta: {title: 'Welcome'}},
         {path: '/login', component: Login, meta: {title: 'Login'}},
         {path: '/register', component: Register, meta: {title: 'Register'}},
-        {path: '/home', component: Home, meta: {title: 'Home'}},
-        {path: '/report', component: Report, meta: {title: 'Report'}},
+        {path: '/home', component: Report, meta: {title: 'Home'}},
+        // {path: '/report', component: Report, meta: {title: 'Report'}},
+        {path: '/budget', component: Budget, meta: {title: 'Budget'}},
+        {path: '/settings', component: Settings, meta: {title: 'Settings'}},
+        {path: '/about-us', component: AboutUs, meta: {title: 'About Us'}},
         {path: '/newscreen', component: Template, meta: {title: 'Template'}},
         {path: '*', component: NotFound, meta: {title: 'Not Found'}} // Render 404 page if url doesn't match other routes
     ]
