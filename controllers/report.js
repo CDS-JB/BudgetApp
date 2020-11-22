cb0998796d16cd9485473175162b129f4f5469cc
CDS-JB <josh.barrell@connecteddata.solutions>
2020-11-22 15:46:24 +0000

    Completed Caclulations

--- controllers/report.js
+++ controllers/report.js
@@ -6,7 +6,8 @@ module.exports = {
             user: {
                 _id: '123456',
                 FirstName: 'John',
-                LastName: 'Doe'
+                LastName: 'Doe',
+                TargetDate: '2021-06-01'
             },
             transactions : [
                 {_id: '1', Date: '2020-10-01', Type: 'Income',  Category: 'Salary',    Amount: 1500.00, StartBalance: 0.00,   EndBalance: 1500.00},
@@ -24,19 +25,19 @@ module.exports = {
 
                 {UserObjectId: '1', PaymentType: 'Income', Source: 'Bank Balance', IncInBudget: 'true', Amount: 4000.00, FrequencyType: 'Balance', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01', Frequency: 'NaN'},
                 {UserObjectId: '1', PaymentType: 'Income', Source: 'Savings', IncInBudget: 'false', Amount: 1000.00, FrequencyType: 'Balance', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01', Frequency: 'NaN'},
-                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2322.00, FrequencyType: 'Lump sum', PaymentStart: '2020-09-26T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2322.00, FrequencyType: 'Lump sum', PaymentStart: '2021-01-12T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2456.00, FrequencyType: 'Lump sum', PaymentStart: '2021-05-15T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Income', Source: 'Part-time work', IncInBudget: 'true', Amount: 48.37, FrequencyType: 'Weekly', PaymentStart: '2020-10-05T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 2, TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Income', Source: 'Bank of M&D', IncInBudget: 'true', Amount: 50, FrequencyType: 'Monthly', PaymentStart: '2020-10-30T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1, TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Example', IncInBudget: 'true', Amount: 20, FrequencyType: 'Weekly', PaymentStart: '2020-10-05T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1, TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Rent', IncInBudget: 'true', Amount: 495, FrequencyType: 'Monthly', PaymentStart: '2020-10-01T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1, TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Overdraft', IncInBudget: 'false', Amount: 557.62, FrequencyType: 'Debt', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Credit Card', IncInBudget: 'true', Amount: 123.21, FrequencyType: 'Debt', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Savings Goal', IncInBudget: 'true', Amount: 1000, FrequencyType: 'Lump sum', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2020-11-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2020-12-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'},
-                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2021-01-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN', TargetDate: '2021-06-01'}
+                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2322.00, FrequencyType: 'Lump sum', PaymentStart: '2020-09-26T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2322.00, FrequencyType: 'Lump sum', PaymentStart: '2021-01-12T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Income', Source: 'Student Finance', IncInBudget: 'true', Amount: 2456.00, FrequencyType: 'Lump sum', PaymentStart: '2021-05-15T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Income', Source: 'Part-time work', IncInBudget: 'true', Amount: 48.37, FrequencyType: 'Weekly', PaymentStart: '2020-10-05T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 2},
+                {UserObjectId: '1', PaymentType: 'Income', Source: 'Bank of M&D', IncInBudget: 'true', Amount: 50, FrequencyType: 'Monthly', PaymentStart: '2020-10-30T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Example', IncInBudget: 'true', Amount: 20, FrequencyType: 'Weekly', PaymentStart: '2020-10-05T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Rent', IncInBudget: 'true', Amount: 495, FrequencyType: 'Monthly', PaymentStart: '2020-10-01T00:00:00.000+00:00', PaymentEnd: '3000-01-01T00:00:00.000+00:00', Frequency: 1},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Overdraft', IncInBudget: 'false', Amount: 557.62, FrequencyType: 'Debt', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Credit Card', IncInBudget: 'true', Amount: 123.21, FrequencyType: 'Debt', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Savings Goal', IncInBudget: 'true', Amount: 1000, FrequencyType: 'Lump sum', PaymentStart: '1970-01-01', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2020-11-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2020-12-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'},
+                {UserObjectId: '1', PaymentType: 'Outcome', Source: 'Tattoo', IncInBudget: 'true', Amount: 200, FrequencyType: 'Lump sum', PaymentStart: '2021-01-04T00:00:00.000+00:00', PaymentEnd: '1970-01-01T00:00:00.000+00:00', Frequency: 'NaN'}
             ]
         }
 
