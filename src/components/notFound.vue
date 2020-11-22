16bcdfb24eea4907b8b3b5bb0061a59b4ac7f669
Tom Frankland <TomFrankland@Toms-MBP.home>
2020-11-22 14:59:38 +0000

    404 css

--- src/components/notFound.vue
+++ src/components/notFound.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>404 Not Found</div>
+    <div>
+        <div class="shadow p-3 mb-5 bg-white rounded col-md" id="error">
+            <div class="d-flex justify-content-center">
+                <img src="/images/404.png" alt="Error Page not Found" id="errorImage">
+            </div>
+            <div class="d-flex justify-content-center">
+                <h1>Page not found!</h1>
+            </div>
+        </div>
+    </div>
 </template>
